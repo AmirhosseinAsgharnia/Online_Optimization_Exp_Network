ce79693d9345edd4c67e3380d593549feeb87ab6
asgha <asgha@amir>
2024-12-14 17:06:18 -0500

    Updated Main and Cost.

--- Main_code.m
+++ Main_code.m
@@ -1,55 +1,78 @@
 clear; clc
-
-lambda = 141;
-
-%%
-MaxIt = 20000;
-T_M = 100;
-
-possible_actions = 5 * ones(1,11);
-num_possible_actions = prod (possible_actions);
-epsilon = 1;
-num_evaluate_actions = ceil(num_possible_actions * epsilon);
-
-num_inputs = 11;
-num_membership_functions = 3;
-num_rules = num_membership_functions ^ num_inputs;
-
-input_bounds = ones (num_inputs , 2);
-input_bounds(: , 2) = possible_actions';
-
-rule_base_cost  = ones (num_rules , 1)/num_rules;
-rule_base_block = ones (num_rules , num_rules)/num_rules;
-rule_base       = ones (num_rules , 1)/num_rules;
-
-f_t = zeros( num_rules , MaxIt);
-F_s = zeros( num_rules , MaxIt);
-
-Repeat_b = zeros(num_rules , T_M);
-Repeat_a = zeros(num_rules , T_M);
 %%
 
-nu = 0.1;
-Beta_LR = .01;
-K = 141;
+load("Four_node.mat")
+load("n_k.mat")
+
+b(:,1) = 1;
+b(:,2) = 2;
+b(:,3) = 0;
+b(:,4) = 1;
+%% Time
+
+time_horizon = 20000; % (T)
+T_M = 100; % Look back time. T_M is used to measure the importance of a reservation based on previous T_M repeat. 
+K = 50; % Time window
 
 %%
-P = zeros(num_rules , MaxIt);
-P(:,1) = 1 / numel(P(:,1));
+Expected_reserve = zeros(time_horizon , 1);
+Expected_block   = zeros(time_horizon , 1);
+%% Core Hyper Parameters
+
+lambda = 141; % Lagrange multiplier (\lambda)
+eta    = sqrt(1/time_horizon); % (\eta)
+v      = 20; % Blocking cost threshold (v)
+epsilon= 0.0; % Exploration rate (\varepsilon)
+Beta_LR= .01; % Learning rate (\beta)
+
+%% Estimator's Hyper Parameters (Router)
+
+router.num                  = 4; % Number of gateway routers
+router.jobs                 = 5;
+router.num_MF               = 3;
+router.num_rules            = router.num_MF ^ router.num;
+router.input_bounds         = ones (router.num , 2);
+router.input_bounds(: , 2)  = router.jobs;
+
+router.rule_base            = zeros (router.num_rules , 1);
+
+%% Estimator's Hyper Parameters (Server)
+
+server.num                  = 11;
+server.jobs                 = 3; % Possible reservations. Action 1: 0 reservations / Action 2: 1 resersavtion / ... / Action 5: 4 reservations
+server.job_vector           = server.jobs * ones( 1 , server.num ); 
+server.num_possible_actions = prod ( server.job_vector );
+server.num_MF               = 2;
+server.num_rules            = server.num_MF ^ server.num;
+server.input_bounds         = ones (server.num , 2);
+server.input_bounds(: , 2)  = server.jobs;
+
+server.rule_base_R          = ones (server.num_rules , 1) / server.num_rules;
+server.rule_base_C          = ones (server.num_rules , router.num_rules) / server.num_rules;
+server.probability          = zeros (time_horizon , 1);
+probability_aux_normalized_pre = ones(server.num_rules , 1) / server.num_rules;
 %%
 
-n = 2;
-m = 5;
+f_t = zeros( server.num_rules , K);
+F_s = zeros( server.num_rules , T_M);
+% 
+% Repeat_b = zeros(num_rules , T_M);
+Repeat_a = zeros(server.num_rules , T_M);
+
+%% Do not touch this part.
+
+n = 1;
+m = server.jobs;
 
 M = zeros(2*n + 1 , m);
 
 for h = 1:m
 
     if h > n && h <= m - n
-        count = 0;
+        counter_1 = 0;
         for j = -n:n
-            count = count + 1;
-            M(count,h) = h + j;
+            counter_1 = counter_1 + 1;
+            M(counter_1,h) = h + j;
         end
     end
 
@@ -59,75 +82,79 @@ for h = 1:n
     M(:,h)  = M(:,n+1);
     M(:,m-h+1) = M(:,m-n);
 end
+
 %%
 
 tic;
-eta = sqrt(1/MaxIt);
-A_i = [1 1 1];
-
-%%
-Expected_reserve = zeros( num_rules , MaxIt);
-Expected_block   = zeros( num_rules , MaxIt);
 rng(111)
-for It = 1 : MaxIt
+
+for It = 1 : time_horizon
 
     if It ~= 1
 
-        [f , c , evaluated_costs] = cost_function(b(It-1 , :) , Cost , Repeat_a ,M);
+        [C , R , evaluated_costs] = cost_function(b(It-1 , :) , Repeat_a , M , epsilon , n_k , server);
 
-        count = 0;
+        counter_1 = 0;
 
         for evaluate_input = evaluated_costs'
 
-            count = count + 1;
-            [H{1:numel([11 11 11])}] = ind2sub([11 11 11] , evaluate_input);
+            counter_1 = counter_1 + 1;
+            [H{1:server.num}] = ind2sub( server.jobs * ones(1 , server.num) , evaluate_input);
             A_eval = cell2mat(H);
 
-            fuzzy_ind_b  = fuzzy_engine_3 (b(It-1 , :) , zeros(num_rules,1) , num_membership_functions , input_bounds);
+            fuzzy_ind_router  = fuzzy_engine_4 (b(It-1 , :) , zeros(router.num_rules , 1) , router.num_MF , router.input_bounds);
 
-            fuzzy_cost  = fuzzy_engine_3 (A_eval , rule_base_cost , num_membership_functions , input_bounds);
-            Reward_cost  = c (count);
-            rule_base_cost (fuzzy_cost.act) = rule_base_cost (fuzzy_cost.act) + Beta_LR * (Reward_cost - fuzzy_cost.res) * fuzzy_cost.phi;
+            fuzzy_R  = fuzzy_engine_11 (A_eval , server.rule_base_R , server.num_MF , server.input_bounds);
+            Reward_cost  = R (counter_1);
+            server.rule_base_R (fuzzy_R.act) = server.rule_base_R (fuzzy_R.act) + Beta_LR * (Reward_cost - fuzzy_R.res) * fuzzy_R.phi;
 
             counter_2 = 1;
-            for h = fuzzy_ind_b.act'
-                fuzzy_block = fuzzy_engine_3 (A_eval , rule_base_block(:,h) , num_membership_functions , input_bounds);
-                Reward_block = f (count);
-                rule_base_block(fuzzy_block.act,h) = rule_base_block (fuzzy_block.act,h) + Beta_LR * (Reward_block - fuzzy_block.res) * fuzzy_block.phi * fuzzy_ind_b.phi(counter_2);
+            
+            for h = fuzzy_ind_router.act'
+                fuzzy_C = fuzzy_engine_11 (A_eval , server.rule_base_C(:,h) , server.num_MF , server.input_bounds);
+                Reward_block = C (counter_1);
+                server.rule_base_C(fuzzy_C.act , h) = server.rule_base_C (fuzzy_C.act,h) + Beta_LR * (Reward_block - fuzzy_C.res) * fuzzy_C.phi * fuzzy_ind_router.phi(counter_2);
                 counter_2 = counter_2 + 1;
             end
 
         end
 
-        counter_2 = 1;
-        for h = fuzzy_ind_b.act'
-            f_t (: , It - 1) =  f_t (: , It - 1) + rule_base_block(:,h)* fuzzy_ind_b.phi(counter_2);
-            counter_2 = counter_2 + 1;
+        f_t (: , 1 : end - 1) = f_t (: , 2 : end);
+        
+        counter_3 = 1;
+        for h = fuzzy_ind_router.act'
+            f_t (: , end) = f_t (: , end) + server.rule_base_C(: , h) * fuzzy_ind_router.phi(counter_3);
+            counter_3 = counter_3 + 1;
         end
-
-        FFS = max(0,min(K,It)^-1 * ( sum (f_t (:,It - min(It,K) + 1: It - 1) , 2)) - nu);
-
-
-        F_s (: , It - 1) = FFS;
-        rule_base = exp(-eta * sum( rule_base_cost + lambda * F_s (: , 1:It - 1) , 2));
-        rule_base_norm = rule_base ./ sum(rule_base);
-
+        
+        FFS = max(0 , min(K , It + 1) ^ -1 * (sum(f_t(: , max(1 , K - It + 2) : K) , 2)) - v);
+        
+        F_s (: , 1 : end - 1) = F_s (: , 2 : end);
+        F_s (: , end) = FFS;
+        
+        probability_aux = exp(- eta * sum(server.rule_base_R + lambda * F_s , 2));
+        probability_aux_normalized = probability_aux ./ sum(probability_aux);
+        
         %
-        Expected_block   (:,It - 1) = f_t(: , It - 1)  .* rule_base_norm;
-        Expected_reserve (:,It - 1) = rule_base_cost .* rule_base_norm;
-        P(:,It) = rule_base_norm;
+        Expected_block   (It - 1) = sum(f_t(:,end) .* probability_aux_normalized);
+        Expected_reserve (It - 1) = sum(server.rule_base_R .* probability_aux_normalized);
+        
+        server.probability(It) = sqrt( sum((probability_aux_normalized_pre - probability_aux_normalized) .^2) );
 
+        probability_aux_normalized_pre = probability_aux_normalized;
+
+        [~ , d] = max(probability_aux_normalized);
+        [X{1:11}] = ind2sub( server.jobs*ones(1,11) , d);
+        Y = cell2mat (X);
+        d = sub2ind( server.jobs*ones(1,11) , Y(11) , Y(10) ,Y(9) ,Y(8) ,Y(7) ,Y(6) ,Y(5) ,Y(4) ,Y(3) ,Y(2) ,Y(1));
+        Repeat_a( : , 1:end-1 ) = Repeat_a( : , 2:end ); Repeat_a( : , end ) = 0;
+        Repeat_a( d , end )     = 1;
     end
-    %%
-
-    Y = sub2ind([11 11 11] , A(It,1)+1 , A(It,2)+1 , A(It,3)+1);
-    Repeat_a( : , 1:end-1 ) = Repeat_a( : , 2:end ); Repeat_a( : , end ) = 0;
-    Repeat_a( Y , end )     = 1;
 
     %%
-
+    
     clc
-    fprintf("The process has been %.3f %% completed! \n",It * 100 / MaxIt)
+    fprintf("The process has been %.3f %% completed! \n",It * 100 / time_horizon)
 
 end
 Time = toc;
--- cost_function.m
+++ cost_function.m
@@ -1,69 +1,124 @@
-function Cost = cost_function (reservations_index,job_request_index)
+function [C , R , evaluated_costs] = cost_function ( b , Repeat_a , M , epsilon , n_k ,server)
 
-reservations = [1 1 1 1 1 1 1 1 1 1 1]';
-job_request  = [2 2 2 2]';
+
+%%
+
+% b            = [2 2 2 2]; 
 W            = [20 20 20 20];
-%%
-
-resource_capacity = [zeros(4,1) ; zeros(4,1) ; reservations];
+job_request  = b';
 
 %%
 
-[distance_vector , distance_index , G] = network_analysis ();
+% Repeat_a = zeros(10000,1); Repeat_a (3250) = 1;
+% epsilon = 0.0;
+
+J = size(M,1);
+r_a = sum (Repeat_a , 2);
+[~ , sort_r_a] = sort (r_a,'descend');
+a_ind = sort_r_a(1:1);
+
+[H{1 : 11}] = ind2sub (server.jobs * ones(1 , 11) , a_ind);
+A_n = cell2mat(H);
+
+B_n = [];
+
+for i = 1:11
+    B_n = [B_n , M(:,A_n(i))];
+end
+
+C_n = [];
+for i = 1:11
+    C_n = [C_n , B_n(n_k( : , i) , i)];
+end
+
+a_max_ind = sub2ind(server.jobs * ones(1 , 11) , C_n(:,11) , C_n(:,10) , C_n(:,9) , C_n(:,8) , C_n(:,7) , C_n(:,6) , C_n(:,5) , C_n(:,4) , C_n(:,3) , C_n(:,2) , C_n(:,1) );
+
+evaluated_costs = a_max_ind;
+
+perm_vec = randperm(numel(evaluated_costs));
+random_select = perm_vec(1 : ceil(epsilon*numel(evaluated_costs)));
+evaluated_costs(random_select) = randi([1 , 362797056],numel(random_select),1);
+
+perm_final = randperm(numel(evaluated_costs));
+evaluated_costs = evaluated_costs(perm_final);
 
 %%
-
+[distance_vector , distance_index , ~] = network_analysis ();
 [x1, x2, x3, x4] = ndgrid(0:job_request(1), 0:job_request(2), 0:job_request(3), 0:job_request(4));
 
 job_request_aux = [x1(:), x2(:), x3(:), x4(:)];
 
-cost_pre = inf;
 
-for job_count = 1 : size(job_request_aux,1)
+evaluated_costs (randperm(ceil(numel(evaluated_costs)*0.999)))=[];
 
-    cost_transfer    = 0;
-    cost_violation   = 0;
 
-    job_request_now = job_request_aux(job_count , :);
-    resource_capacity_now = resource_capacity;
+C = zeros(numel (evaluated_costs) , 1);
+R = zeros(numel (evaluated_costs) , 1);
 
-    for count = 1 : numel (distance_vector)
 
-        if job_request_now(distance_index (count , 1)) ~= 0 && resource_capacity_now(distance_index (count , 2)) ~= 0
 
-            D = min(job_request_now(distance_index (count , 1))  , resource_capacity_now(distance_index (count , 2)));
+counter_1 = 0;
+for instance = evaluated_costs'
+    
+    counter_1 = counter_1 + 1;
+    [H{1 : 11}] = ind2sub (server.jobs * ones(1 , 11) , instance);
+    reservations = cell2mat(H)';
+    
+    %%
 
-            job_request_now(distance_index (count , 1)) = job_request_now(distance_index (count , 1)) - D;
-            resource_capacity_now(distance_index (count , 2)) = resource_capacity_now(distance_index (count , 2)) - D;
+    resource_capacity = [zeros(4,1) ; zeros(4,1) ; reservations];
 
-            cost_transfer = cost_transfer + D * distance_vector(count);
+    %%
+
+
+
+    cost_pre = inf;
+
+    for job_count = 1 : size(job_request_aux,1)
+
+        cost_transfer    = 0;
+        cost_violation   = 0;
+
+        job_request_now = job_request_aux(job_count , :);
+        resource_capacity_now = resource_capacity;
+
+        for count = 1 : numel (distance_vector)
+
+            if job_request_now(distance_index (count , 1)) ~= 0 && resource_capacity_now(distance_index (count , 2)) ~= 0
+
+                D = min(job_request_now(distance_index (count , 1))  , resource_capacity_now(distance_index (count , 2)));
+
+                job_request_now(distance_index (count , 1)) = job_request_now(distance_index (count , 1)) - D;
+                resource_capacity_now(distance_index (count , 2)) = resource_capacity_now(distance_index (count , 2)) - D;
+
+                cost_transfer = cost_transfer + D * distance_vector(count);
+
+            end
+
+            if sum(job_request_now) == 0 || sum(resource_capacity_now) == 0
+                break;
+            end
 
         end
 
-        if sum(job_request_now) == 0 || sum(resource_capacity_now) == 0
-            break;
+        for count = 1:numel(job_request_now)
+
+            job_request_diff = job_request(count) - (job_request_aux(job_count , count) - job_request_now(count) );
+
+            cost_violation = cost_violation + W(count) * job_request_diff;
+
         end
 
+        cost_reservation = sum(reservations);
+
+        if cost_transfer + cost_violation < cost_pre
+            cost_pre = cost_transfer + cost_violation;
+            Cost_T = cost_transfer;
+            Cost_V = cost_violation;
+            Cost_R = cost_reservation;
+        end
     end
 
-    for count = 1:numel(job_request_now)
-
-        job_request_diff = job_request(count) - (job_request_aux(job_count , count) - job_request_now(count) );
-
-        cost_violation = cost_violation + W(count) * job_request_diff;
-
-    end
-
-    cost_reservation = sum(reservations);
-
-    if cost_transfer + cost_violation < cost_pre
-        cost_pre = cost_transfer + cost_violation;
-        Cost_T = cost_transfer;
-        Cost_V = cost_violation;
-        Cost_R = cost_reservation;
-    end
+    C(counter_1) = Cost_T + Cost_V;
+    R(counter_1) = Cost_R;
 end
-
-Cost.T = Cost_T; 
-Cost.V = Cost_V; 
-Cost.R = Cost_R; 
